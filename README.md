f4a3d7ccdbe5e3c6631ec7ffaec0d8cc8c892609
shilfol <yayananoda@gmail.com>
2017-10-19 01:45:01 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+#### reference
+http://kwmt27.net/2017/03/23/how-to-deploy-go-application-on-heroku/
+https://1000ch.net/posts/2016/heroku-automatic-deploy.html
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+)
+
+func main() {
+    port := os.Getenv("PORT")
+    if port == "" {
+        log.Fatal("$PORT must be set")
+    }
+
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":"+port,nil)
+}
+
+func handler(w http.ResponseWrite, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %q", r.URL.Path[1:])
+
+}
